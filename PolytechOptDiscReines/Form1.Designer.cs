c9f64276582285bf7e4645d07e5df8b749e1399f
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-15 17:59:44 +0100

    first draw

--- PolytechOptDiscReines/Form1.Designer.cs
+++ PolytechOptDiscReines/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
-            this.lblSoluce = new System.Windows.Forms.Label();
+            this.dgvBoard = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBoard)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
@@ -42,33 +43,33 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // lblSoluce
+            // dgvBoard
             // 
-            this.lblSoluce.AutoSize = true;
-            this.lblSoluce.Location = new System.Drawing.Point(119, 115);
-            this.lblSoluce.Name = "lblSoluce";
-            this.lblSoluce.Size = new System.Drawing.Size(35, 13);
-            this.lblSoluce.TabIndex = 1;
-            this.lblSoluce.Text = "label1";
+            this.dgvBoard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBoard.Location = new System.Drawing.Point(48, 102);
+            this.dgvBoard.Name = "dgvBoard";
+            this.dgvBoard.Size = new System.Drawing.Size(530, 349);
+            this.dgvBoard.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.lblSoluce);
+            this.ClientSize = new System.Drawing.Size(814, 525);
+            this.Controls.Add(this.dgvBoard);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBoard)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Label lblSoluce;
+        private System.Windows.Forms.DataGridView dgvBoard;
     }
 }
 
--- PolytechOptDiscReines/Form1.cs
+++ PolytechOptDiscReines/Form1.cs
@@ -13,6 +13,10 @@ namespace PolytechOptDiscReines
 {
     public partial class Form1 : Form
     {
+        private int n = 8;
+        private int m = 8;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +30,32 @@ namespace PolytechOptDiscReines
             int n2 = 50;
 
             SimulatedAnnealing algo1 = new SimulatedAnnealing(x0, t0, n1, n2);
-            
+
             Board result = algo1.start();
+            this.updateDgv(result);
             var x = result.finesseNbQueensConflicting();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvBoard.AutoGenerateColumns = false;
+            for (int i = 1; i <= n; i++)
+            {
+                dgvBoard.Columns.Add("col" + i, "column " + i);
+                dgvBoard.Columns[i - 1].FillWeight = 1;
+            }
+            for (int j = 0; j < m; j++)
+                dgvBoard.Rows.Add();
+            dgvBoard.Rows.Add(m);
+        }
+
+        private void updateDgv(Board board) {
+            int[] tab = board.getPositions();
+            for (int i = 0; i < tab.Count(); i++)
+            {
+                this.dgvBoard.Rows[i].Cells[tab[i] -1].Style.BackColor = Color.Green;
+                this.dgvBoard.Rows[i].Cells[tab[i] -1].Style.ForeColor = Color.Green;
+            }
         }
     }
 }
